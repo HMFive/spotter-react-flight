c903b12e36802deab03b1275c7891dde968cf31a
yusufciftci <yusufciftci@outlook.com.tr>
2025-02-10 00:16:18 +0300

    add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import {Provider} from "react-redux";
 import {createTheme, ThemeProvider} from "@mui/material";
 import {store} from "./store/store.ts";
 import {Home} from "./pages/home.tsx";
+
 const theme = createTheme({
 
 });
@@ -16,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
               <Home></Home>
           </ThemeProvider>
       </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
