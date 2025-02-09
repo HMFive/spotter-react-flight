b48b7af0057d140fdd55b9b934186362c280b758
yusufciftci <yusufciftci@outlook.com.tr>
2025-02-09 15:18:06 +0300

    create redux store

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {createTheme, ThemeProvider} from "@mui/material";
+import {store} from "./store/store.ts";
+const theme = createTheme({
 
+});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Provider store={store}>
+          <ThemeProvider theme={theme}>
+              <App></App>
+          </ThemeProvider>
+      </Provider>
   </StrictMode>,
 )
